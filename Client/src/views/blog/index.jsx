93a36e01c910b6adebef388dd6b7367be97edfb3
OfficialShak <82374250+ShakaLondon@users.noreply.github.com>
2021-07-29 01:57:22 +0100

    add pdf download button

--- Client/src/views/blog/index.jsx
+++ Client/src/views/blog/index.jsx
@@ -107,6 +107,20 @@ componentDidUpdate = async (prevProps) => {
 
             <div dangerouslySetInnerHTML={{ __html: this.state.blog.content }}></div>
           </Container>
+          <Form.Group className="d-flex mt-3 justify-content-end">
+            <Button
+              type="submit"
+              size="lg"
+              variant="dark"
+              style={{ marginLeft: "1em" }}
+              onClick={e => this.handleSubmit(e)}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-short" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"/>
+</svg>
+              <span>Download PDF</span>
+            </Button>
+          </Form.Group>
         </div>
       );
     }
