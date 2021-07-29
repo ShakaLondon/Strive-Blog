0ab4c6b802bcc2e4b18d045fdb339014868b9be8
OfficialShak <82374250+ShakaLondon@users.noreply.github.com>
2021-07-30 00:01:37 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -36,7 +36,7 @@ const __dirname = dirname(__filename);
 
 // import { getCurrentFolderPath } from "../src/lib/fs-tools.js"
 
-const publicDirectory = path.join(__dirname, 'Client');
+const publicDirectory = path.join(__dirname, 'public');
 
 
 
