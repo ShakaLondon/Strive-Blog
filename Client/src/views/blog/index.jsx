9e9ba1b64b5d06717ba7038a5cf9594c9cbf4def
OfficialShak <82374250+ShakaLondon@users.noreply.github.com>
2021-07-30 12:41:37 +0100

    fix pdf download

--- Client/src/views/blog/index.jsx
+++ Client/src/views/blog/index.jsx
@@ -117,7 +117,7 @@ componentDidUpdate = async (prevProps) => {
               className="d-flex"
               // style={{ marginLeft: "1em" }}
               // onClick={e => this.handleSubmit(e)}
-              href={`${apiURL}/${id}/pdf`}
+              href={`${apiURL}/blogs/${id}/pdf`}
             >
               <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-down-short" viewBox="0 0 13 13">
   <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"/>
