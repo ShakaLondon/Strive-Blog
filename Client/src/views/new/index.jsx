b4fcf899ee8e456fc54c9c295719f3665a9b679d
OfficialShak <82374250+ShakaLondon@users.noreply.github.com>
2021-08-02 23:06:29 +0100

    Update index.jsx

--- Client/src/views/new/index.jsx
+++ Client/src/views/new/index.jsx
@@ -131,7 +131,7 @@ export default class NewBlogPost extends Component {
           body: JSON.stringify(this.state.blogPost),
           headers: {
               'Content-type': 'application/json',
-              'Access-Control-Allow-Origin': 'https://strive-blog-m5.herokuapp.com'
+              'Access-Control-Allow-Origin': 'https://strive-blog-m5.herokuapp.com/blogs'
           }
       })
       console.log(response.ok)
